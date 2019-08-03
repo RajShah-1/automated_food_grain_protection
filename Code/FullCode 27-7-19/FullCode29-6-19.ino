514c5f0c8b642b58ac76f097b3900830dc327fac
RajShah-1 <srk13@iitbbs.ac.in>
2019-08-03 18:18:25 +0530

    Stabilized app

--- Code/FullCode 27-7-19/FullCode29-6-19.ino
+++ Code/FullCode 27-7-19/FullCode29-6-19.ino
@@ -1,24 +1,33 @@
+// Install DHT sensor library by Adafruit V1.3.4 & Adafruit Unified Sensor V1.0.3 
+ 
+#include <DHT.h>
+#include <DHT_U.h>
+
 #define TEMPERATURE_PIN A1
 #define RAIN_PIN A0
 #define MOISTURE_PIN A2
+#define DHTPIN 2
+
+#define DHTTYPE DHT11
 
 String OnOff;   
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup(){
   Serial.begin(9600);
+  dht.begin();
 }
 
 /*
  * Pins occupied :
- *   Temp Sensor : A1,GND,5V;
- *   Rain Sensor : A0,GND,5V
- *   Bluetooth Module : 0(RX),1(TX),3.3V,GND
- *   Moisture Sensor : A2,GND,5V;
- *   Add Data for other sensors:
- *   
+ *   Temp Sensor : A1, GND, 5V;
+ *   Rain Sensor : A0, GND, 5V
+ *   Bluetooth Module : 0(RX), 1(TX), 3.3V, GND
+ *   Moisture Sensor : A2, GND, 5V;
+ *   Humidity Sensor : 2, GND, 3.3v
  */
 void loop(){
-  OnOff = "";//Resets string a 
+  OnOff = "";//Resets string OnOff 
   while (Serial.available()){ 
     delay(10); 
     char c = Serial.read(); 
@@ -27,7 +36,7 @@ void loop(){
   
   if(OnOff.equals("OFF")){
     while(!Serial.available()){
-      Serial.println("|||");
+      Serial.println("-_-_-_-");
       delay(1000);
     }
   }
@@ -41,16 +50,21 @@ void loop(){
 
 void printData(void){
   // "|" is used as a delimeter
-  Serial.println(String(getTemperature())+"|"+String(getHumidity())+"|"+String(isRaining())+"|"+String(getMoisture()));
+  Serial.println(String(getTemperature())+"_"+String(getHumidity())+"_"+String(isRaining())+"_"+String(getMoisture()));
 }
 //Returns temperature in C
 int getTemperature(void){
-  int temp = analogRead(TEMPERATURE_PIN);
-  return temp * 0.48828125;   // convert output (mv) to readable celcius
+ float t = dht.readTemperature();
+  return (int)(t+0.5);   
 }
 //Returns humidity in %
 int getHumidity(void){
-  return 1;
+  float h = dht.readHumidity();
+  if(isnan(h)){
+    return -1; //Returns -1 if it fails to read the value
+  }
+  return (int)(h+0.5);
+  
 }
 //Returns current status of rain
 String isRaining(void)
@@ -68,7 +82,8 @@ String isRaining(void)
 }
 //Returns soil moisture in %
 int getMoisture(void){
-  int tmpMoisture = analogRead(MOISTURE_PIN);
-  int moisture = map(tmpMoisture,550,0,0,100);
+  float tmpMoisture = analogRead(MOISTURE_PIN);
+  //int moisture = map(tmpMoisture,1023,440,0,100);
+  int moisture = (int)((1 - tmpMoisture/1023.0)*100 + 30);
   return moisture;
 }
